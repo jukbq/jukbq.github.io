6dca48762b519ade2a65b474617e6d53f42db27d
Yurii Ohlii <jukbq77@gmail.com>
2022-02-07 19:13:40 +0200

    New app

--- modul-6-js-advanced-updated/m6-hw1/src/app/app.component.ts
+++ modul-6-js-advanced-updated/m6-hw1/src/app/app.component.ts
@@ -1,10 +1,16 @@
+<<<<<<< Updated upstream:modul-6-js-advanced-updated/m6-hw1/src/app/app.component.ts
 import { Component } from '@angular/core';
+=======
+import { Component, OnInit } from '@angular/core';
+
+>>>>>>> Stashed changes:modul-6-js-advanced-updated/m6hw1/src/app/app.component.ts
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+<<<<<<< Updated upstream:modul-6-js-advanced-updated/m6-hw1/src/app/app.component.ts
   title = 'my-app';
 
 
@@ -18,10 +24,78 @@ export class AppComponent {
 
 
 
+=======
+  title = 'm6hw1';
+  public bw_list = ''
+  public bw_input = ''
+  public text_input = ''
+  public inp_bw = 'Word here...'
+  public inp_text: string = 'Text here'
+  public bad_words: any = []
+
+
+
+  constructor() { }
+
+  clear_bw() {
+    this.inp_bw = ''
+    this.bw_input = ''
+  }
+
+  clear_text() {
+    this.inp_text = ''
+  }
+
+  addBad(): void {
+    let bw = this.inp_bw;
+    if (bw !== '' && !bw.match(/\s/)) {
+      this.bw_input = ''
+      this.bad_words.push(bw)
+      this.inp_bw = 'Word here...'
+      this.bw_list = ''
+      for (let i = 0; i < this.bad_words.length; i++) {
+        this.bw_list += this.bad_words[i];
+        this.bw_list += ',  ';
+      }
+
+    } else {
+      this.bw_input = 'valid'
+
+    }
+  }
+  cenzor(): any {
+    let ti = this.inp_text
+
+    if (ti !== '') {
+      this.bad_words.forEach((word: { [Symbol.replace](string: string, replacer: (substring: string, ...args: any[]) => string): string; }) => {
+        ti = ti.replace(word, function (mach: any) {
+          let x = mach.length;
+          let y = '';
+          for (let i = 0; i < x; i++) {
+            y += "*";
+          }
+          return y;
+        });
+      })
+      this.inp_text = ti
+    }
+  }
+
+
+  reset() {
+    location.reload()
+  }
+>>>>>>> Stashed changes:modul-6-js-advanced-updated/m6hw1/src/app/app.component.ts
 
 
 
 
+<<<<<<< Updated upstream:modul-6-js-advanced-updated/m6-hw1/src/app/app.component.ts
+=======
+}
+function removeClass(arg0: string): string {
+  throw new Error('Function not implemented.');
+>>>>>>> Stashed changes:modul-6-js-advanced-updated/m6hw1/src/app/app.component.ts
 }
 
 
--- modul-6-js-advanced-updated/m6-hw1/src/app/app.module.ts
+++ modul-6-js-advanced-updated/m6-hw1/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Hw1Component } from './hw1/hw1.component';
+
+
 
 
 
@@ -22,7 +26,12 @@ import { Hw1Component } from './hw1/hw1.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+<<<<<<< Updated upstream:modul-6-js-advanced-updated/m6-hw1/src/app/app.module.ts
     FormsModule
+=======
+    FormsModule,
+    ReactiveFormsModule
+>>>>>>> Stashed changes:modul-6-js-advanced-updated/m6hw1/src/app/app.module.ts
   ],
   providers: [],
   bootstrap: [AppComponent]
