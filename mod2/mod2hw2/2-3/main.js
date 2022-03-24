b7d4f012b3dafe82dcbf928820571937742d57f6
Yurii Ohlii <jukbq77@gmail.com>
2022-03-24 18:10:40 +0200

    mod2hw2 2-3

---
+++ mod2/mod2hw2/2-3/main.js
@@ -0,0 +1,11 @@
+let numm = +prompt("Введіть число від 1 до 12");
+
+if(numm==12 ||  numm==1 || numm == 2){
+    alert("Зима");
+}else if(numm==3 ||  numm==4 || numm == 5){
+    alert("Весна");
+}else if(numm==6 ||  numm==7 || numm == 8){
+    alert("Літо");
+}else if(numm==9 ||  numm==10 || numm == 11){
+    alert("Осінь");
+}
