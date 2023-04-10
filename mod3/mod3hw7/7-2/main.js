223119e8d91e2f96d0d22aabd5e6285a1ddea544
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2023-04-10 10:24:19 +0300

    мод3 дз 7-2

--- mod3/mod3hw7/7-2/main.js
+++ mod3/mod3hw7/7-2/main.js
@@ -1,25 +1,40 @@
+let CapsLockOn = false;
+
 window.addEventListener('keydown', function (event) {
-    let push = event.code
-    console.log(event)
-    let key = document.getElementsByTagName('li')
-    let alt = event.altKey
-    for (const i of key) {
-        if (i.className == push) {
-            i.style.backgroundColor = '#411111'
-            i.style.boxShadow = 'none'
-        }
+    let push = event.code;
+
+    let key = document.getElementsByTagName('li');
+    let alt = event.altKey;
+    if (alt) {
+        event.preventDefault();
+    }
+    if (push === 'CapsLock') {
+        CapsLockOn = !CapsLockOn;
+        console.log(CapsLockOn);
     }
 
-})
+    for (const i of key) {
+        if (i.className === push) {
+            i.style.backgroundColor = '#411111';
+            i.style.boxShadow = 'none';
+        }
+    }
+    if (push === "Tab") {
+        event.preventDefault();
+    }
+});
 
 window.addEventListener('keyup', function (event) {
-    let push = event.code
-    let key = document.getElementsByTagName('li')
-    let x = ""
+    let push = event.code;
+    let key = document.getElementsByTagName('li');
     for (const i of key) {
-        if (i.className == push) {
-            i.style.backgroundColor = 'rgb(190, 12, 12)'
+        if (i.className === push && push !== 'CapsLock') {
+            i.style.backgroundColor = 'rgb(190, 12, 12)';
+            i.style.boxShadow = '2px 2px 3px  rgb(187, 16, 16)';
+        }
+        if (push === 'CapsLock' && !CapsLockOn) {
+            i.style.backgroundColor = 'rgb(190, 12, 12)';
             i.style.boxShadow = '2px 2px 3px  rgb(187, 16, 16)'
         }
     }
-})
+});
