68c59fb48490f0fa1553ab581ed30ad5d94419b8
Yurii Ohlii <61588029+jukbq@users.noreply.github.com>
2023-04-13 14:47:21 +0300

    мод 3 дз 16-3

--- mod3/mod3hw16/16-3/main.js
+++ mod3/mod3hw16/16-3/main.js
@@ -1,42 +1,43 @@
 $(function () {
-    $('.box').on('click', function () {
-        let top;
-        let left;
-        let x = $(this).offset()
-        top = x.top;
-        left = x.left
-        $(this).addClass('bigBox')
-        $('.fon').css({
-            backgroundColor: '#140707c9',
-            zIndex: 2
-        })
-        $(this).animate({
-            width: '300px',
-            height: '300px',
-            top: '90px',
-            left: '410px'
-        }, 1500)
+    let top;
+    let left;
 
-        $('.bigBox').on('click', function () {
-                $(this).animate({
-                top: `${top}`,
-                left: `${left}`,
+    $('.box').on('click', function () {
+        if ($(this).hasClass('bigBox')) {
+            $(this).stop().animate({
                 width: '150px',
                 height: '150px',
+                top: top,
+                left: left,
+                marginTop: 0,
+                marginLeft: 0,
             }, 1500, function () {
                 $(this).removeClass('bigBox');
-                $('.fon').css('backgroundColor', '#ffffff00')
-                $('.fon').css('zIndex', '-1')
-                
-            })
+                $('.fon').css({
+                    display: 'none',
+                    zIndex: -1
+                });
+            });
+        } else {
+            let x = $(this).offset();
+            top = x.top;
+            left = x.left;
 
+            $(this).addClass('bigBox');
 
-        })
-    })
+            $('.fon').css({
+                display: 'block',
+                zIndex: 2
+            });
 
-
-
-
-
-
-})
+            $(this).stop().animate({
+                width: '500px',
+                height: '500px',
+                top: '50%',
+                left: '50%',
+                marginTop: '-250px',
+                marginLeft: '-250px',
+            }, 1500);
+        }
+    });
+});
